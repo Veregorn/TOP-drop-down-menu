129cd4375bff3167ed075f7f0a60fed9c44f695e
Raul Jimenez <rjbarco@gmail.com>
2022-11-07 11:19:08 +0100

    Added another submenu

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import "./styles.css";
 
 const aParent = document.querySelectorAll(".a_parent");
+const ddMenuA = document.querySelectorAll(".dd_menu_a");
 
 aParent.forEach((aParentItem) => {
-  aParentItem.addEventListener('click', () => {
+  aParentItem.addEventListener("click", () => {
+    ddMenuA.forEach((ddMenuAItem2) => {
+      ddMenuAItem2.classList.remove("active");
+    });
     aParent.forEach((aParentItem2) => {
       aParentItem2.classList.remove("active");
     });
     aParentItem.classList.add("active");
+  });
+});
+
+ddMenuA.forEach((ddMenuAItem) => {
+  ddMenuAItem.addEventListener("click", () => {
+    ddMenuA.forEach((ddMenuAItem2) => {
+      ddMenuAItem2.classList.remove("active");
+    });
+    ddMenuAItem.classList.add("active");
   });
 });
